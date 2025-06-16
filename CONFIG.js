84e01a1ec936397d2922597b9d35ca9612029b3f
datdepzai12 <datc8322@gmail.com>
2025-06-16 23:28:50 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -2,7 +2,7 @@ const CONFIGDATA = {
   titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
   descriptionHeader:
     "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
+  buttonYes: "Tớ đồngsad
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
